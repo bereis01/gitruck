5a6f165763b2ca632e3f0fbdce24ac2f6ebd5f99
Bernardo Reis de Almeida <bernardo.reis.almeida01@gmail.com>
2025-11-15 18:28:37 -0300

    Modularized (kinda?) the gitruck module

--- src/gitruck.py
+++ src/gitruck.py
@@ -13,196 +13,41 @@ class Gitruck:
     def __init__(self, owner: str, repo: str):
         self.owner = owner
         self.repo = repo
-        self.url = f"https://github.com/{owner}/{repo}.git"
-        self.path = "./tmp"
 
-    def commit_dist(self):
-        # Retrieves information from github api
-        response = requests.get(
-            "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/commits"
+        self._github_repository_url = f"https://github.com/{owner}/{repo}.git"
+        self._local_repository_path = "./tmp"
+
+        self._load_repository_locally()
+
+    def _load_repository_locally(self):
+        if os.path.exists(self._local_repository_path):
+            shutil.rmtree(self._local_repository_path)
+        self.conn = Repo.clone_from(
+            self._github_repository_url, self._local_repository_path
         )
-        commits = response.json()
 
-        # Parses it
-        commit_authors = []
-        commit_distribution = {}
-        for commit in commits:
-            author = commit["author"]["login"]
-            commit_authors.append(author)
-            if author not in commit_distribution.keys():
-                commit_distribution[author] = 1
+    def _get_all_file_objects(self):
+        files = []
+        self._recursively_get_file_objects(self.conn.head.commit.tree, files)
+
+        return files
+
+    def _recursively_get_file_objects(root, files, level=0):
+        for entry in root:
+            if entry.type == "tree":
+                print_files_from_git(entry, files, level + 1)
             else:
-                commit_distribution[author] += 1
-
-        # Generates the visualization
-        img = BytesIO()
-        fig, ax = plt.subplots()
-        fig.set_size_inches(8, 6)
-        ax.hist(commit_authors)
-        fig.savefig(img, format="png", dpi=300)
-        plt.close()
-
-        return img
+                files.append(entry)
 
     def calculate_truck_factor(self):
-        # Clones repository
-        if os.path.exists(self.path):
-            shutil.rmtree(self.path)
-        self.conn = Repo.clone_from(self.url, self.path)
-
-        # Gets files
-        # Uses linguist to get the files
-        git_cmd = self.conn.git
-        raw_files = git_cmd.execute(["github-linguist", "-b"])
-
-        # Removes the top analysis
-        raw_files = raw_files.split("\n")
-        for i in range(len(raw_files)):
-            if raw_files[i] == "":
-                raw_files = raw_files[i + 1 :]
-                break
-
-        # Gets the names of the files
-        _files = []
-        for line in raw_files:
-            if "  " in line:
-                _files.append(line.strip())
-
-        def print_files_from_git(root, files, level=0):
-            for entry in root:
-                if entry.type == "tree":
-                    print_files_from_git(entry, files, level + 1)
-                else:
-                    if entry.path in _files:
-                        files.append(entry)
-
-        files = []
-        print_files_from_git(self.conn.head.commit.tree, files)
-
-        self.files = files
-
-        # Gets contributors
-        git_cmd = self.conn.git
-        contributors = git_cmd.execute(["git", "shortlog", "-sne", "--all"])
-
-        contributors = contributors.split("\n")
-        for i in range(len(contributors)):
-            contributor = contributors[i]
-            count, identifier = contributor.strip().split("\t")
-            count = int(count)
-            name, email = identifier.split("<")
-            name = name.strip()
-            email = email[:-1].strip()
-            contributors[i] = tuple([count, name, email])
-
-        self.contributors = contributors
-
-        # Parses contributors
-        # Groups emails
-        grouped_emails = {}
-        for contributor in contributors:
-            if contributor[2] not in grouped_emails.keys():
-                grouped_emails[contributor[2]] = [contributor[1]]
-            else:
-                grouped_emails[contributor[2]].append(contributor[1])
-
-        # Groups names
-        grouped_names = []
-        while len(grouped_emails.keys()) > 0:
-            reference_group = grouped_emails.pop(list(grouped_emails.keys())[0])
-            # Expands the reference group with other groups
-            for key in list(grouped_emails.keys()):
-                comparison_group = grouped_emails[key]
-                # Checks if there is a pair of names in both groups
-                # that have a Levenshtein distance of 1 or less
-                for lhs_name in reference_group:
-                    for rhs_name in comparison_group:
-                        if Levenshtein.distance(lhs_name, rhs_name) <= 1:
-                            # Unites both groups
-                            reference_group += grouped_emails.pop(key)
-                            break
-                    else:  # Bad code to make the inner loop break propagate to the outer loop
-                        continue
-                    break
-            grouped_names.append(reference_group)
-
-        # Creates a mapped-dev-name
-        dev_name = {}
-        for name_group in grouped_names:
-            root_name = name_group[0]
-            for name in name_group:
-                dev_name[name] = root_name
-
-        self.dev_name = dev_name
-
-        # Gets commits per file
-        commits_per_filepath = {}
-        for file in files:
-            commits_for_file_generator = self.conn.iter_commits(all=True, paths=file.path)
-            commits_per_filepath[file.path] = list(commits_for_file_generator)
-
-        self.commits_per_filepath = commits_per_filepath
-
-        # Calculates DOA and normalized DOA
-        files = _files
+        files = self._get_code_file_paths()
+        dev_name = self._generate_dev_names(self._get_contributors())
         contributors = list(set(dev_name.values()))
+        commits_per_file = self._get_commits_per_file(files)
 
-        # Defines DOA data structure
-        DOA = {}
-        for contributor in contributors:
-            DOA[contributor] = {}
-            for file in files:
-                DOA[contributor][file] = 0.0
+        DOA = self._calculate_DOA(files, contributors, commits_per_file, dev_name)
+        normalized_DOA = self._calculate_normalized_DOA(DOA, files, contributors)
 
-        # Calculates DOA
-        for contributor in contributors:
-            for file in files:
-                # Authorship
-                authorship = 0
-                first_author = commits_per_filepath[file][-1].author.name
-                if (first_author in dev_name.keys()) and (
-                    dev_name[commits_per_filepath[file][-1].author.name] == contributor
-                ):
-                    authorship = 1
-
-                # Deliveries
-                deliveries = 0
-                acceptances = 0
-                for commit in commits_per_filepath[file]:
-                    if (commit.author.name in dev_name.keys()) and (
-                        dev_name[commit.author.name] == contributor
-                    ):
-                        deliveries += 1
-                    else:
-                        acceptances += 1
-
-                # Final value
-                DOA[contributor][file] = (
-                    3.293
-                    + 1.098 * authorship
-                    + 0.164 * deliveries
-                    - 0.321 * math.log(1 + acceptances)
-                )
-
-        # Normalizes DOA
-        normalized_DOA = copy.deepcopy(DOA)
-        for file in files:
-            # Gets minimum and maximum values
-            max_val = -1
-            min_val = 999999
-            for contributor in contributors:
-                if DOA[contributor][file] > max_val:
-                    max_val = DOA[contributor][file]
-                if DOA[contributor][file] < min_val:
-                    min_val = DOA[contributor][file]
-
-            # Normalizes each value
-            for contributor in contributors:
-                normalized_DOA[contributor][file] = (
-                    DOA[contributor][file] - min_val
-                ) / (max_val - min_val)
-
-        # Calculates authored files
         # Parameters
         k = 0.75
         m = 3.293
@@ -225,7 +70,6 @@ class Gitruck:
             )
         }
 
-        # Calculates the truck factor
         # Calculates the superset of files
         _files = []
         for contributor in authored_files.keys():
@@ -243,10 +87,160 @@ class Gitruck:
             authored_files.pop(next(iter(authored_files)), None)
             truck_factor += 1
 
-            # Recalculates file set
+            # Recalculates superset of files
             _files = []
             for contributor in authored_files.keys():
                 _files += authored_files[contributor]
             _files = list(set(_files))
 
         return truck_factor
+
+    def _get_code_file_paths(self):
+        # Uses linguist to get the files
+        git_cmd = self.conn.git
+        raw_files = git_cmd.execute(["github-linguist", "-b"])
+
+        # Removes the top analysis
+        raw_files = raw_files.split("\n")
+        for i in range(len(raw_files)):
+            if raw_files[i] == "":
+                raw_files = raw_files[i + 1 :]
+                break
+
+        # Gets the names of the files
+        files = []
+        for line in raw_files:
+            if "  " in line:
+                files.append(line.strip())
+
+        return files
+
+    def _get_contributors(self):
+        # Uses the git interface to get this
+        git_cmd = self.conn.git
+        contributors = git_cmd.execute(["git", "shortlog", "-sne", "--all"])
+
+        # Parses the result
+        contributors = contributors.split("\n")
+        for i in range(len(contributors)):
+            contributor = contributors[i]
+            count, identifier = contributor.strip().split("\t")
+            count = int(
+                count
+            )  # Count refers to amount of commits made by the contributor
+            name, email = identifier.split("<")
+            name = name.strip()
+            email = email[:-1].strip()
+            contributors[i] = tuple([count, name, email])
+
+        return contributors
+
+    def _generate_dev_names(self, contributors: list):
+        # Groups names by same email
+        grouped_emails = {}
+        for contributor in contributors:
+            if contributor[2] not in grouped_emails.keys():
+                grouped_emails[contributor[2]] = [contributor[1]]
+            else:
+                grouped_emails[contributor[2]].append(contributor[1])
+
+        # Unites groups of names from the previous step
+        # based on their similarity
+        grouped_names = []
+        while len(grouped_emails.keys()) > 0:
+            reference_group = grouped_emails.pop(next(iter(grouped_emails)))
+            # Expands the reference group with other groups
+            for key in list(grouped_emails.keys()):
+                comparison_group = grouped_emails[key]
+                # Checks if there is a pair of names in both groups
+                # that have a Levenshtein distance of 1 or less
+                for lhs_name in reference_group:
+                    for rhs_name in comparison_group:
+                        if Levenshtein.distance(lhs_name, rhs_name) <= 1:
+                            # Unites both groups
+                            reference_group += grouped_emails.pop(key)
+                            break
+                    else:  # Bad code to make the inner loop break propagate to the outer loop
+                        continue
+                    break
+            grouped_names.append(reference_group)
+
+        # Maps the names in each group
+        # to a unique identifier
+        dev_name = {}
+        for name_group in grouped_names:
+            root_name = name_group[0]
+            for name in name_group:
+                dev_name[name] = root_name
+
+        return dev_name
+
+    def _get_commits_per_file(self, files: list):
+        commits_per_file = {}
+        for file in files:
+            commits_for_file_generator = self.conn.iter_commits(all=True, paths=file)
+            commits_per_file[file] = list(commits_for_file_generator)
+
+        return commits_per_file
+
+    def _calculate_DOA(
+        self, files: list, contributors: list, commits_per_file: dict, dev_name: dict
+    ):
+        # Defines DOA data structure
+        DOA = {}
+        for contributor in contributors:
+            DOA[contributor] = {}
+            for file in files:
+                DOA[contributor][file] = 0.0
+
+        # Calculates DOA
+        for contributor in contributors:
+            for file in files:
+                # Authorship
+                authorship = 0
+                first_author = commits_per_file[file][-1].author.name
+                if (first_author in dev_name.keys()) and (
+                    dev_name[commits_per_file[file][-1].author.name] == contributor
+                ):
+                    authorship = 1
+
+                # Deliveries
+                deliveries = 0
+                acceptances = 0
+                for commit in commits_per_file[file]:
+                    if (commit.author.name in dev_name.keys()) and (
+                        dev_name[commit.author.name] == contributor
+                    ):
+                        deliveries += 1
+                    else:
+                        acceptances += 1
+
+                # Final value
+                DOA[contributor][file] = (
+                    3.293
+                    + 1.098 * authorship
+                    + 0.164 * deliveries
+                    - 0.321 * math.log(1 + acceptances)
+                )
+
+        return DOA
+
+    def _calculate_normalized_DOA(self, DOA: dict, files: list, contributors: list):
+        normalized_DOA = copy.deepcopy(DOA)
+        for file in files:
+            # Gets minimum and maximum values
+            max_val = -1
+            min_val = 999999
+            for contributor in contributors:
+                if DOA[contributor][file] > max_val:
+                    max_val = DOA[contributor][file]
+                if DOA[contributor][file] < min_val:
+                    min_val = DOA[contributor][file]
+
+            # Normalizes each value
+            for contributor in contributors:
+                normalized_DOA[contributor][file] = (
+                    DOA[contributor][file] - min_val
+                ) / (max_val - min_val)
+
+        return normalized_DOA
