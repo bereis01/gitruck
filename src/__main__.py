77abde7601b3a1c0040da56e9b66a0c21e095e4b
Bernardo Reis de Almeida <bernardo.reis.almeida01@gmail.com>
2025-11-16 16:39:53 -0300

    Minor ajustments

--- src/__main__.py
+++ src/__main__.py
@@ -2,12 +2,13 @@ from .html import Html
 from .args import ArgParse
 from .gitruck import Gitruck
 
-html = Html()
 args = ArgParse()
-gitruck = Gitruck(args.get_url())
+git_url = args.get_url()
 
+gitruck = Gitruck(args.get_url())
 truck_factor, top_contributors = gitruck.calculate_truck_factor()
 
+html = Html()
 html.add_truck_factor(truck_factor)
 html.add_top_contributors(top_contributors)
 html.persist()
